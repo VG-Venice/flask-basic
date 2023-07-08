c54e0e179e746c1ca7044b09bac32c7c32d0f4b4
VG-Venice <68179331+VG-Venice@users.noreply.github.com>
2023-07-08 16:40:31 +0000

    Added correct db details

--- database.py
+++ database.py
@@ -1,3 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://gzfn5fwokybwa2axcgsq:pscale_pw_EKWqz1hIhwXx8KCwyFwd7bRPcAoG1rQDRNN0JcUCC6c@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4")
+db_connection_string = "mysql+pymysql://4z5xony1qnse9hpmnt5n:pscale_pw_Iuo2iVOi2Y41JtZtDMtHgxB2F2r4X1VqgZXUyEF504U@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                      connect_args={
+                        "ssl": {
+                          "ssl_ca": "/etc/ssl/cert.pem"
+                        }
+                      })
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from jobs"))
+  print(result.all())
