24a6ccb82b91a2159dad28d480f86e93688b397c
VG-Venice <68179331+VG-Venice@users.noreply.github.com>
2023-07-08 18:15:43 +0000

    working properly  and 100%

--- database.py
+++ database.py
@@ -1,14 +1,26 @@
+from flask import jsonify
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://4z5xony1qnse9hpmnt5n:pscale_pw_Iuo2iVOi2Y41JtZtDMtHgxB2F2r4X1VqgZXUyEF504U@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://x9em91lw3prh5g69dt64:pscale_pw_Mo6OhAdAdEsYJuEA4VM0teT5EMmW0itovCdDgoLR3mx@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
-                      connect_args={
-                        "ssl": {
-                          "ssl_ca": "/etc/ssl/cert.pem"
-                        }
-                      })
+                       connect_args={"ssl": {
+                         "ssl_ca": "cert.pem"
+                       }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  print(result.all())
+
+def ListJobs():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(({
+      "id": row.id,
+      "title": row.title,
+      "location": row.location,
+      "salary": row.salary,
+      "currency": row.currency,
+      "responsbilities": row.responsbilities,
+      "requirements": row.requirements,
+    }))
+  return result_dicts
--- main.py
+++ main.py
@@ -1,41 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
+from database import ListJobs
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id' : 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': '₹ 10,20,000'
-  },
-    {
-    'id' : 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': '₹ 11,40,000'
-  },
-    {
-    'id' : 3,
-    'title': 'Back-end Developer',
-    'location': 'London, UK',
-    'salary': '£ 78,000'
-  },
-    {
-    'id' : 4,
-    'title': 'Cleaner',
-    'location': 'Kolkata, India',
-    'salary': '₹ 3,00,000'
-  }
-]
 
 @app.route('/')
 def index():
-    return render_template('home.html', jobs = JOBS, company_name="Baconz")
+  JOBS = ListJobs()
+  return render_template('home.html', jobs=JOBS, company_name="Baconz")
+
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
-  
-app.run(host='0.0.0.0', port=81, debug=True)
+  return ListJobs()
 
+
+app.run(host='0.0.0.0', port=81, debug=True)
