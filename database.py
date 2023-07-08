86ab72d422fdc687991bd54d2c6c1b61791cd15e
VG-Venice <68179331+VG-Venice@users.noreply.github.com>
2023-07-08 12:09:56 +0000

    Connected database to replit

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://gzfn5fwokybwa2axcgsq:pscale_pw_EKWqz1hIhwXx8KCwyFwd7bRPcAoG1rQDRNN0JcUCC6c@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4")
