dfd8316a1fe555c78418cf4343b2770d45d8c8ec
VG-Venice <68179331+VG-Venice@users.noreply.github.com>
2023-07-10 10:20:48 +0000

    new connection details

--- database.py
+++ database.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine, text
 
 # database: flaskpythontutorial
-# username: q09yhgb1jx6ne6hhwfxw
+# username: j6ihuy8jobfk1vxa6tny
 # host: aws.connect.psdb.cloud
-# password: pscale_pw_XOd4H2TvG7OKjPFOLydezLcD1wcLOpE089GNu2Rudgx
+# password: pscale_pw_KQHnbCcsTKlZ9fgeNtXHlP6pkPl0tUhcYK9KZS8Utf1
 
-engine = create_engine("mysql+pymysql://q09yhgb1jx6ne6hhwfxw:pscale_pw_XOd4H2TvG7OKjPFOLydezLcD1wcLOpE089GNu2Rudgx@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4",
-                       connect_args={"ssl": {
-                         "ssl_ca": "cert.pem"
-                       }})
+
+engine = create_engine(
+  "mysql+pymysql://j6ihuy8jobfk1vxa6tny:pscale_pw_KQHnbCcsTKlZ9fgeNtXHlP6pkPl0tUhcYK9KZS8Utf1@aws.connect.psdb.cloud/flaskpythontutorial?charset=utf8mb4",
+  connect_args={"ssl": {
+    "ssl_ca": "cert.pem"
+  }})
 
 
 def ListJobs():
@@ -27,6 +29,7 @@ def ListJobs():
     }))
   return result_dicts
 
+
 '''def ListJobs1(id):
   id = jobs['id']
   with engine.connect() as conn:
@@ -43,7 +46,6 @@ def ListJobs():
       "requirements": row.requirements,
     }))
   return result_dicts'''
-
 '''def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
@@ -56,22 +58,21 @@ def ListJobs():
     else:
       return dict(rows[0])'''
 
+
 def loadthe_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id ={id}")
-    )
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id ={id}"))
     rows = []
     for row in result.all():
       rows.append(({
-      "id": row.id,
-      "title": row.title,
-      "location": row.location,
-      "salary": row.salary,
-      "currency": row.currency,
-      "responsbilities": row.responsbilities,
-      "requirements": row.requirements,
-    }))
+        "id": row.id,
+        "title": row.title,
+        "location": row.location,
+        "salary": row.salary,
+        "currency": row.currency,
+        "responsbilities": row.responsbilities,
+        "requirements": row.requirements,
+      }))
     if len(rows) == 0:
       return None
     else:
